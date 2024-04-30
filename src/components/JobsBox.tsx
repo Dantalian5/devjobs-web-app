9429b6b135b7ccfd1904864765a114a24195eb9a
Bruno Moleta <brunomoleta@pm.me>
2024-04-30 18:36:57 -0300

    :label: type JobsBox component

--- src/components/JobsBox.tsx
+++ src/components/JobsBox.tsx
@@ -1,6 +1,8 @@
 import JobCard from "@/components/JobCard";
-function JobsBox({ data }: any) {
-  const jobs = data.map((job: any) => <JobCard key={job.id} job={job} />);
+import { Job } from "@/type/jobs";
+
+function JobsBox({ data }: { data: Job[] }) {
+  const jobs = data.map((job: Job) => <JobCard key={job.id} job={job} />);
   return <div className="l-jobs-wrapper">{jobs}</div>;
 }
 
