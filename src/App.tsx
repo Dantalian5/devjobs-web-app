b1a63724af09ded9ff5f14591d2bc1b69af72b2c
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-05-09 09:44:31 +0200

    Add Zustand for state management | by brunomoleta
    - Add Zustand for state management
    - Move color schema state to a zustand store
    - Create and initialice a localStorage variable for scheme initial load

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { useColorSchemeStore } from '@/store/colorScheme.store';
 import Header from '@/components/Header';
 import Jobs from '@/components/Jobs';
 
-import { useColorSchemeStore } from '@/store/colorScheme.store';
-
-type Theme = 'dark' | 'light';
-
 function App() {
-  const [colorScheme, setColorScheme] = useState<Theme>('light');
-  useEffect(() => {
-    setColorScheme(() => {
-      const localScheme = localStorage.getItem('colorScheme');
-
-      switch (localScheme) {
-        case 'light':
-          return 'light';
-        case 'dark':
-          return 'dark';
-        default:
-          return window.matchMedia('(prefers-color-scheme: dark)').matches
-            ? 'dark'
-            : 'light';
-      }
-    });
-  }, []);
-  function toggleColorScheme() {
-    const newColor: Theme = colorScheme === 'light' ? 'dark' : 'light';
-    localStorage.setItem('colorScheme', newColor);
-    setColorScheme(newColor);
-  }
-
+  const { colorScheme, toggleColorScheme } = useColorSchemeStore();
   return (
     <div className={`main-container theme-${colorScheme}`}>
       <BrowserRouter basename='/'>
--- src/store/colorScheme.store.ts
+++ src/store/colorScheme.store.ts
@@ -1,11 +1,26 @@
 import { create } from 'zustand';
 
-const useColorSchemeStore = create((set) => ({
-  colorScheme: 'light',
-  toggleColorScheme: () =>
-    set((state) => ({
-      colorScheme: state.colorScheme === 'light' ? 'dark' : 'light',
-    })),
+interface ColorScheme {
+  colorScheme: 'light' | 'dark';
+  toggleColorScheme: () => void;
+}
+
+const useColorSchemeStore = create<ColorScheme>((set, get) => ({
+  colorScheme: (() => {
+    const localScheme = localStorage.getItem('colorScheme');
+    if (localScheme === 'light' || localScheme === 'dark') {
+      return localScheme;
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  })(),
+  toggleColorScheme: () => {
+    const { colorScheme } = get();
+    const newColorScheme = colorScheme === 'light' ? 'dark' : 'light';
+    set({ colorScheme: newColorScheme });
+    localStorage.setItem('colorScheme', newColorScheme);
+  },
 }));
 
 export { useColorSchemeStore };
