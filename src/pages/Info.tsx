68fb4088af8af919d97635949eca10527bec67ac
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-05-20 14:42:31 +0200

    Fix link clasess (broken flex)

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/layout/Header';
 import InfoView from '@/components/common/InfoView';
 
 const Info = () => {
-  console.log('Info');
   return (
     <div>
       <Header />
